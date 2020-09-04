8c5bdd28742e4a892ea9f9f755adac7563c05e70
akhidwivedi <akhil8825@gmail.com>
2020-09-05 03:43:48 +0530

    first commit

---
+++ Myapi/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import  Hero
+
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class  Meta:
+        model = Hero
+        fields = ('name','alias')
+
+# from rest_framework import serializers
+
+# from .models import Hero
+
+# class HeroSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Hero
+#         fields = ('name', 'alias')
